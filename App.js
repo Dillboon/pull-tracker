b35b1f05ae6cbb1503c05e927097c03756dd61cf
Dillboon <zdf46@yahoo.com>
2026-04-27 19:13:04 -0400

    Test2exportoverhaul

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import 'react-native-get-random-values';
 import 'react-native-gesture-handler';
 import React, { useState, useEffect, useCallback, useRef } from 'react';
 import { View, SafeAreaView, TouchableOpacity, Text, StyleSheet } from 'react-native';
